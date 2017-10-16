e7a04ae4ff481f9563448270ecd625d3a5a544c7
campaignhumza <humzasheikh@live.co.uk>
2017-10-16 11:47:07 +0100

    split function implemented

--- macroprocessor/MP.hs
+++ macroprocessor/MP.hs
@@ -1,6 +1,7 @@
 module MP where
 
 import System.Environment
+import Data.Char
 
 type FileContents = String
 
@@ -14,10 +15,34 @@ separators
 
 
 lookUp :: String -> [(String, a)] -> [a]
-lookUp = error "TODO: implement lookUp"
+lookUp str table  = [ y | (str',y) <- table, str == str']    
+
+
+{--split :: [Char] -> String -> (String, [String])
+split seps str = split' seps str [] [] []
+      where
+        split' seps' [] sepsacc wordacc words = (sepsacc,words)
+        split' seps' (x:xs) sepsacc wordacc words
+          | isAlpha x = split' seps' xs sepsacc (x:wordacc) words
+          | isWS x || elem x seps' = split' seps' xs (x:sepsacc) [] (wordacc:words)
+--}
 
 split :: [Char] -> String -> (String, [String])
-split = error "TODO: implement split"
+split seps [] = ("",[""])
+split seps (y:ys)
+    | isAlpha y = (seps', ((y:w):ws))
+    | isWS y = (y:seps', "" : (w:ws))
+    | elem y seps = (y:seps',"" : (w:ws))
+    where
+      (seps', (w:ws)) = split seps ys
+
+--isAlpha :: Char -> Bool
+--isAlpha a = (ord 'a' <= a && a <= ord 'z') || (ord 'A' <= a && a <= ord 'Z') 
+
+
+isWS :: Char -> Bool
+isWS a = a == ' '
+
 
 combine :: String -> [String] -> [String]
 combine = error "TODO: implement combine"
